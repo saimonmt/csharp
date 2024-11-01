030418f776b8382fe352c072bd77b3cdeffe74b9
saimonmt <saimonfer@gmail.com>
2024-11-01 16:30:48 -0400

    Tipos de dados Nao Primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -17,3 +17,44 @@ char inicial = 'W';
 // Tipos Logicos
 bool estaLogado = true;
 bool ativo = false;
+
+//  Tipos de dados Nao Primitivos
+string saudacao = "Ola mundo!";
+
+// Arrays
+int[] numeros = { 1, 2, 3, 4, 5};
+Console.WriteLine(numeros);
+string[] nomes = { "Maria", "Jose"};
+Console.WriteLine(nomes);
+// Classes
+public class Pessoa
+{
+    public string Nome;
+    public int idade;
+}
+
+// Structs
+public class Ponto
+{
+    public int X;
+    public int Y;
+}
+
+// Enums
+public enum diaDaSemana
+{
+    Segunda, Terca, Quarta, Quinta, Sexta, Sabado, Domingo
+}
+
+// Interfaces
+public interface IVeiculo
+{
+    void Mover();
+}
+
+// Delegates
+public void Delegates Processar(int numero);
+
+// Nullable types
+int? idade = null;
+

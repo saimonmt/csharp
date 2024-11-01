c9a3cc2b2752ee8b551e1206d5ed59ebb2f5f497
saimonmt <saimonfer@gmail.com>
2024-11-01 17:15:23 -0400

    Operadores Aritmeticos
    Operadores Aritmeticos 01/11/2024

--- operadores/Program.cs
+++ operadores/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Operadores Aritmeticos 
+double resultado = 12 + 8 + 12 / 3 ;
+Console.WriteLine(resultado);
+
+double res2 = (12 + 8 + 12) / 3 ;
+Console.WriteLine(res2);
+
+double res3 = (2 + 8 ) * 2 / 4 ;
+Console.WriteLine(res3);
+

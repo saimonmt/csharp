a3fa41c7f3ed117f80c5609271e85afdcef9c35f
saimonmt <saimonfer@gmail.com>
2024-11-04 17:20:28 -0400

    Metodo Parse
    Metodo Parse

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -9,3 +9,18 @@ Console.WriteLine(saudacao);
 string nome1 = "Jose ";
 string saudacao1 = $"Ola, {nome1}! Bem-vindo.";
 Console.WriteLine(saudacao1);
+
+// Operador GetType()
+int num = 5;
+Console.WriteLine(num.GetType() + " Usado para obter o tipo de instancia de um objeto em tempo de execucao");
+
+// Palavra-chave typeof
+Console.WriteLine(typeof(int) + " Usado para obter o tipo de uma classe ou estrutura em tempo de compilacao");
+
+//  Metodo Parse
+string valorTexto = "100";
+Console.WriteLine("A variavel valorTexto e do tipo: "+ valorTexto.GetType());
+
+int num2 = int.Parse(valorTexto);
+Console.WriteLine(num2);
+Console.WriteLine("A variavel numeroParse é do Tipo: " + num2.GetType());

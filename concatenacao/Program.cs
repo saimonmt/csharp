8a7d85f11ced76053d69fe22515ca323907f5d5b
saimonmt <saimonfer@gmail.com>
2024-11-04 16:55:22 -0400

    Projeto sobre Concatenacao
    Adicionado Projeto sobre Concatenacao

---
+++ concatenacao/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//  Concatenacao 04/11/2024
+string nome = "Joao ";
+string saudacao = "Ola, "+nome+"! Bem-vindo.";
+Console.WriteLine(saudacao);

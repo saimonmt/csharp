30cafb9965269d11105613529b9a35f0daf5d610
saimonmt <saimonfer@gmail.com>
2024-11-04 16:59:55 -0400

    Interpolacao Strings
    Exemplo Interpolacao Strings

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -4,3 +4,8 @@ Console.WriteLine("Hello, World!");
 string nome = "Joao ";
 string saudacao = "Ola, "+nome+"! Bem-vindo.";
 Console.WriteLine(saudacao);
+
+// Interpolacao de Strings
+string nome1 = "Jose ";
+string saudacao1 = $"Ola, {nome1}! Bem-vindo.";
+Console.WriteLine(saudacao1);

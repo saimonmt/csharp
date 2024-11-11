cecad7e7c7391a8eac61ed7609c28e4638600b57
saimonmt <saimonfer@gmail.com>
2024-11-11 14:32:10 -0400

    Estrutura de Repeticao
    Adicionado Estrutura de Repeticao While.

---
+++ estruturas-repeticao/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Estrutura de Repeticao while 11/11/2024;
+Random numeroAleatorio = new Random();
+int numero = numeroAleatorio.Next(1,101);
+int contador = 0;
+Console.WriteLine($"Numero Algo: {numero}");
+// Loop que incrementa o contador ate ele atingir o numero alvo
+while (contador != numero)
+{
+    Console.WriteLine($"Contador: {contador}.");
+    contador++;
+}
+Console.WriteLine($"Contador atingiu o numero alvo: {contador}");
